d16ee35c52b66ea8c2931fa9b2477b8813b78137
Xenuku <50945740+Xenuku@users.noreply.github.com>
2022-10-02 22:22:20 +1000

    Update credits page with emails, start GameOver Fix PlayGame ground layer being offset Clean up spawnmanager a bit

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,32 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public bool enableSpawn = true;
-    public GameObject Enemy;
 
-    public int enemyNum;
+    public int enemyNum = 1;
 
     public GameObject[] EnemyTypes;
     public GameObject[] BigEnemyTypes;
 
     public Transform[] spawnPoint;
 
-    public int respawnTime;
-    public int currentTime;
-    public int increaseRespawn;
+    public int respawnTime = 15; //every 3 seconds
+    private int currentTime;
+    public int increaseRespawn = 30;
     public int reset;
-    public int enemy_num_limit;
+    public int enemy_num_limit = 14;
     //public randomX_1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        enemy_num_limit = 14;
-
-        increaseRespawn = 30;
-        enemyNum = 1;
         currentTime = 0;
-        respawnTime = 15; //every 3 seconds
         InvokeRepeating("AddSecond", 20f, 1f);
         InvokeRepeating("SpawnSystem", 1f, 0.5f);
         //InvokeRepeating("manageEnemyNum",5f,1f);
@@ -44,32 +38,20 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnEnemy()
     {
-
-
         int ranEnemy = Random.Range(0, EnemyTypes.Length);
-
-        int boss=Random.Range(0,10);
-
-
+        int boss = Random.Range(0, 10);
         float randomX = Random.Range(Random.Range(Camera.main.transform.position.x - 50.0f, Camera.main.transform.position.x - 40.0f), Random.Range(Camera.main.transform.position.x + 50.0f, Camera.main.transform.position.x + 40.0f));
-
-
-
-        //float randomY =Random.Range(Camera.main.transform.position.y-15.0f, Camera.main.transform.position.y+15.0f); 
-
         float randomY = Random.Range(Random.Range(Camera.main.transform.position.y - 50.0f, Camera.main.transform.position.y - 40.0f), Random.Range(Camera.main.transform.position.y + 50.0f, Camera.main.transform.position.y + 40.0f));
-
-
         if (enableSpawn)
         {
             GameObject enemy = (GameObject)Instantiate(EnemyTypes[ranEnemy], new Vector2(randomX, randomY), Quaternion.identity);
             enemyNum += 1;
         }
-        int ranBigEnemy= Random.Range(0,BigEnemyTypes.Length);
+        int ranBigEnemy = Random.Range(0, BigEnemyTypes.Length);
         Debug.Log(boss);
-
-        if(enableSpawn &boss<3){
-            GameObject BigEnemy=    (GameObject)Instantiate(BigEnemyTypes[ranBigEnemy], new Vector2(randomX, randomY), Quaternion.identity);
+        if (enableSpawn & boss < 3)
+        {
+            GameObject BigEnemy = (GameObject)Instantiate(BigEnemyTypes[ranBigEnemy], new Vector2(randomX, randomY), Quaternion.identity);
         }
     }
     void AddSecond()
@@ -78,40 +60,31 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnSystem()
     {
-        //Debug.Log(currentTime);
-        //Debug.Log(currentTime&respawnTime);
         if (Mathf.Floor(currentTime & respawnTime) == 0)
         { //for respawn time
             SpawnEnemy();
-            //Debug.Log("spawnenemy working");
         }
         if (Mathf.Floor(currentTime & increaseRespawn) == 0)
         {
             respawnTime -= 1;
             enemy_num_limit += 1;
-            //Debug.Log(respawnTime);
         }
         if (Mathf.Floor(currentTime & reset) == 0)
         {
             respawnTime = 15;
-            //Debug.Log(respawnTime);
         }
 
     }
     void manageEnemyNum()
     {
-        //Debug.Log(Mathf.Floor(currentTime&increaseRespawn)==0);
-
         if (enemyNum > enemy_num_limit)
         {
             enableSpawn = false;
-
         }
         else if (enemyNum <= enemy_num_limit + 1)
         {
             enableSpawn = true;
         }
-        //Debug.Log(enemyNum);
     }
     void reduceEnemy(int die)
     {
